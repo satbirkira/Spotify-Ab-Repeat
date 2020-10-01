cfcf5e1a485c858a5a13df42f5566dc407f7a0ea
Satbir Saini <satbir.kira@gmail.com>
2020-10-01 10:10:31 -0400

    Starting to select doms of interest

--- content.js
+++ content.js
@@ -1,15 +1,29 @@
 /* Listen for messages */
-alert("Loaded content");
+// alert("Loaded content");
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     
-	alert(msg);
+
     if (msg && (msg == "iloveyou")) {
 		
 		var playButton = $(".player-controls__buttons .control-button:eq( 2 )")
 		console.log(playButton);
 		playButton.click()
 		
+		var progressBar = $(".progress-bar:eq( 0 )")
+		console.log(progressBar)
+		
+		alert(progressBar.offset().top)
+		alert(progressBar.offset().left)
+		alert(progressBar.offset().left + progressBar.width())
+		
+		var trackLength = $(".playback-bar__progress-time:eq(1)")
+		console.log(trackLength)
+		alert(trackLength.text())
+		
+		var trackCurrentTime = $(".playback-bar__progress-time:eq(0)")
+		console.log(trackCurrentTime)
+		alert(trackCurrentTime.text())
 		
 		
 		sendResponse("Done");
--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ changeColor.onclick = function(element) {
 	
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, "iloveyou", function(response){
-			alert(response)
+			console.log(response) // will show if you inspect element the popup and go console
 		});
 	});
 };
